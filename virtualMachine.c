6a8df3a7afcc1b9ffe6647d545e19ddd3f6a786f
EthanPittman171 <ethanpittman171@gmail.com>
2023-05-11 16:15:33 -0400

    Added 64KB of memory for 16-bit architecture

--- virtualMachine.c
+++ virtualMachine.c
@@ -1,6 +1,11 @@
+#include <stdint.h>
 #include <stdio.h>
+
+#define MAX_MEMORY (1 << 16)  // 16-bits (65536 in decimal)
 
 int main(int argc, char *argv[])
 {
+    uint16_t memory[MAX_MEMORY];  // 16-bit memory for VM (64 KB)
+
     return 0;
 }
