301cb4ccd694a8d7dd6e90f8e91e30eef2f4e299
EthanPittman171 <ethanpittman171@gmail.com>
2023-05-12 12:11:07 -0400

    Created general flow of control for running VM

--- virtualMachine.c
+++ virtualMachine.c
@@ -3,6 +3,8 @@
 
 #define MAX_MEMORY (1 << 16)  // 16-bits (65536 in decimal)
 
+uint16_t mem_read(uint16_t address);
+
 int main(int argc, char *argv[])
 {
     // Registers
@@ -49,6 +51,74 @@ int main(int argc, char *argv[])
 
     uint16_t memory[MAX_MEMORY];  // 16-bit memory for VM (64 KB)
     uint16_t reg[R_COUNT];        // 16-bit registers
+
+    reg[R_COND] = FL_ZRO;  // Set initial condition flag
+
+    // Set initial value of PC
+    // 0x3000 will be the starting point
+    uint16_t PC_START = 0x3000;
+    reg[R_PC] = PC_START;
+
+    int running = 1;
+    while (running) {
+        // Fetch the instruction from the PC
+        uint16_t instruction = mem_read(reg[R_PC]++);
+        uint16_t opCode = instruction >> 12;
+
+        switch (opCode) {
+            case OP_BR:
+                // Implement code for Branch
+                break;
+            case OP_ADD:
+                // Implement code for Add
+                break;
+            case OP_LD:
+                // Implement code for Load
+                break;
+            case OP_ST:
+                // Implement code for Store
+                break;
+            case OP_JSR:
+                // Implement code for Jump Register
+                break;
+            case OP_AND:
+                // Implement code for Bitwise AND
+                break;
+            case OP_LDR:
+                // Implement code for Load Register
+                break;
+            case OP_STR:
+                // Implement code for Store Register
+                break;
+            case OP_RTI:
+                // Unused
+                break;
+            case OP_NOT:
+                // Implement code for Bitwise NOT
+                break;
+            case OP_LDI:
+                // Implement code for Load Indirect
+                break;
+            case OP_STI:
+                // Implement code for Store Indirect
+                break;
+            case OP_JMP:
+                // Implement code for Jump
+                break;
+            case OP_RES:
+                // Implement code for Reserved (Unused)
+                break;
+            case OP_LEA:
+                // Implement code for Load Effective Address
+                break;
+            case OP_TRAP:
+                // Implement code for Execute Trap
+                break;
+            default:
+                // Implement code for a bad op code
+                break;
+        }
+    }
 
     return 0;
 }
